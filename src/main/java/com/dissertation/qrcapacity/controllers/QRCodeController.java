cb175a2f94dbaeaa7a889854acd79b6a9198feb7
Masaba Gerald <masabagerald@gmail.com>
2023-07-05 00:01:54 +0300

    Adding QR code generator service

--- src/main/java/com/dissertation/qrcapacity/controllers/QRCodeController.java
+++ src/main/java/com/dissertation/qrcapacity/controllers/QRCodeController.java
@@ -1,2 +1,31 @@
-package com.dissertation.qrcapacity.controllers;public class QRCodeController {
+package com.dissertation.qrcapacity.controllers;
+
+
+import com.dissertation.qrcapacity.services.QRCodeGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@Controller
+public class QRCodeController {
+
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping(value ="/genrateAndDownloadQRCode/{codeText}/{width}/{height}")
+    public void download(
+            @PathVariable("codeText") String codeText,
+            @PathVariable("width") Integer width,
+            @PathVariable("height") Integer height)
+            throws Exception {
+        QRCodeGenerator.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
+    }
+
+
+
 }
--- src/main/java/com/dissertation/qrcapacity/services/QRCodeGenerator.java
+++ src/main/java/com/dissertation/qrcapacity/services/QRCodeGenerator.java
@@ -1,2 +1,48 @@
-package com.dissertation.qrcapacity.services;public class QRCodeGenerator {
+package com.dissertation.qrcapacity.services;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeGenerator {
+
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
+    }
+
+    public static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+
+    public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }
 }
